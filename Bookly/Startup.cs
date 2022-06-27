162486e203dad7809cd3331fdf647ae3fbdc1ff3
saesbyeol <leo.lavric@racunarstvo.hr>
2022-06-27 23:07:27 +0200

    Add project files.

---
+++ Bookly/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Bookly.Startup))]
+namespace Bookly
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
