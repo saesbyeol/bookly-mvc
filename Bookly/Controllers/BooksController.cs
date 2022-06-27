1ae1e78ae1c038b3aa984f871fa9fc271452cd7f
saesbyeol <leo.lavric@racunarstvo.hr>
2022-06-27 23:21:59 +0200

    added added db

---
+++ Bookly/Controllers/BooksController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Bookly.Models;
+using Bookly.ViewModels;
+
+namespace Bookly.Controllers
+{
+    public class BooksController : Controller
+    {
+        // GET: Books/Random
+        public ActionResult Random()
+        {
+            var book = new Book() { Name = "Shrek!" };
+            var customers = new List<Customer>
+            {
+                new Customer {Name = "Customer 1"},
+                new Customer {Name = "Customer 2"}
+
+            };
+            var viewModel = new RandomBookViewModel
+            {
+                Book = book,
+                Customers = customers
+            };
+
+            return View(viewModel);
+        }
+
+    }
+}
---
+++ Bookly/ViewModels/RandomBookViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Bookly.Models;
+
+namespace Bookly.ViewModels
+{
+    public class RandomBookViewModel
+    {
+        public Book Book { get; set; }
+        public List<Customer> Customers { get; set; }
+    }
+}
